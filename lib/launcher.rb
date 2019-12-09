efe5b91077a30c5e6d1f784c48eee8c34c3725ae
David FABREGUETTE <david@macbook-pro-de-david.home>
2019-12-09 11:16:11 +0100

    added : launcher logic

--- lib/launcher.rb
+++ lib/launcher.rb
@@ -3,9 +3,62 @@ require 'awesome_print'
 require 'rdoc'
 require 'rspec'
 require 'require_all'
+require 'singleton'
 
 module CodeWars
   class Launcher
+    include Singleton
+
+    WELCOME_TEXT = "Welcome to CodeWars Game !\n~~~"
+
+    # Initilaze the app, gets the first event to
+    # pass it on and get the game on !
+    def initialize
+
+      self.class.load_all_files
+
+      CodeWars::DataStore.instance
+
+      require 'readline'
+      require "awesome_print"
+
+      start
+
+      #
+      # ap "Before anything - enter your name :"
+      # current_player = Readline.readline('❞ ', true)
+      #
+      # ap "Hey #{current_player} ! Let's get into this ..."
+
+    end
+
+    # Starts the game ! Shows the welcome section & launches the first event
+    def start
+      # Show the welcome section
+      welcome
+
+      # Get the first event
+      event = CodeWars::DataStore.instance.events.first
+      launch(event)
+    end
+
+    # Shows the game logo & welcome text
+    def welcome
+      logo = File.read("app/assets/logo.txt")
+      print logo
+
+      puts self.class::WELCOME_TEXT
+    end
+
+    # Launches the given event the the screen
+    # @param Event event The event to be launched on screen
+    # @return Event Next event to be launched on screen
+    def launch(event)
+      ap event.label
+      # current_player = Readline.readline('❞ ', true)
+    end
+
+    private
 
     DIRS_TO_LOAD = %w(config extensions models)
 
@@ -14,28 +67,6 @@ module CodeWars
       DIRS_TO_LOAD.each do |dir_to_load|
         require_all "./app/#{dir_to_load}"
       end
-    end
-
-    def initialize
-
-      self.class.load_all_files
-
-      CodeWars::DataStore.instance.seed
-
-      puts "what ??"
-      require 'readline'
-      require "awesome_print"
-
-      logo = File.read("app/assets/logo.txt")
-      print logo
-
-      ap "Welcome to CodeWars Game !"
-      ap "---"
-      ap "Before anything - enter your name :"
-      current_player = Readline.readline('❞ ', true)
-
-      ap "Hey #{current_player} ! Let's get into this ..."
-
     end
   end
 end
---
+++ spec/lib/launcher_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe "Launcher" do
+  describe "#initialize" do
+    it "launches welcome part !" do
+      event = CodeWars::DataStore.instance.events.first
+      expect do
+        CodeWars::Launcher.instance
+      end
+      .to output(/#{CodeWars::Launcher::WELCOME_TEXT}/).to_stdout
+    end
+  end
+
+  describe "#start" do
+    it "launches first event part !" do
+      event = CodeWars::DataStore.instance.events.first
+      expect do
+        CodeWars::Launcher.instance.start
+      end
+      .to output(/#{event.label}/).to_stdout
+    end
+  end
+
+  describe "#welcome" do
+    it "displays logo and welcome text" do
+      logo = File.read("app/assets/logo.txt")
+      launcher = nil
+
+      # Disable console outputs temporarily
+      RSpec::Mocks.with_temporary_scope do
+        %w(puts print).each do |p_cmd|
+          allow_any_instance_of(CodeWars::Launcher).to receive(p_cmd.to_sym)
+          .and_return(true)
+        end
+        launcher = CodeWars::Launcher.instance
+      end
+
+      # Checks console output
+      expect do
+        launcher = launcher.welcome
+      end.to output(/#{logo + CodeWars::Launcher::WELCOME_TEXT}/).to_stdout
+    end
+  end
+end

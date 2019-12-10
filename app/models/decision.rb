f81b2b316101c308fd1f3499d0c131eea0799e43
David FABREGUETTE <david@macbook-pro-de-david.home>
2019-12-10 06:44:55 +0100

    added : #update_player AND #next_event proxy

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -2,10 +2,36 @@ module CodeWars
   class Decision < CodeWarsModel
     attr_accessor :label
 
-    # The event id which the decision will lead to
-    attr_accessor :leading_event_id
+    # OPTIONAL The event slug which the decision will lead to
+    # If left empty, decision will lead to raw static next event
+    # Be aware this attribute can be overidden by events#next_event_slug
+    # see events#next_event_slug comment
+    attr_accessor :next_event_slug
 
-    # The event id by which the decision was raised
-    attr_accessor :decided_event_id
+    # The event slug by which the decision was raised
+    attr_accessor :decided_event_slug
+
+    # OPTIONAL If set - Event will ask the player for a custom input
+    # (instead of a string choice) and will store the response to the
+    # given current player model attribute
+    # @example
+    # "#name" will take the player input and store it to current player's #name attribute
+    attr_accessor :current_player_input_attribute
+
+    # Stores the time at which the decision was made by player
+    attr_accessor :chosen_at
+
+    def update_player(player_input)
+      if CodeWars::Player.instance.respond_to? current_player_input_attribute
+        CodeWars::Player.instance.update({
+          current_player_input_attribute => player_input
+        })
+      end
+    end
+
+    def next_event
+      CodeWars::DataStore.instance.events.select{|e| e.slug == self.next_event_slug}.first
+    end
+
   end
 end

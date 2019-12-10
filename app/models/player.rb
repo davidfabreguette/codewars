7be9e4149641ebb5012f5b14709b9889e839d1ff
David FABREGUETTE <david@macbook-pro-de-david.home>
2019-12-10 06:46:24 +0100

    Made the player a singleton (as it's the one and only player !) + #update method

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,22 @@
+require "singleton"
 module CodeWars
   ##
   # This class represents the player
   class Player < CodeWarsModel
+    include Singleton
 
     # Name
     attr_accessor :name
+
+    # This methods sets attributes based on given hash attributes
+    # @param Hash attributes
+    def update(attributes)
+      attributes.each do |attribute, value|
+        if self.respond_to? attribute
+          CodeWars::Player.instance.send("#{attribute}=", value)
+        end
+      end
+    end
 
   end
 end

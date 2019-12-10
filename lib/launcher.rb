3dc9e5019c3fa57d6019cf7e8b05400c9aa67710
David FABREGUETTE <david@macbook-pro-de-david.home>
2019-12-10 22:05:31 +0100

    launcher structure with specs

--- lib/launcher.rb
+++ lib/launcher.rb
@@ -48,57 +48,44 @@ module CodeWars
       launch(event)
     end
 
-    # Shows the game logo & welcome text
-    def welcome
-      logo = File.read("app/assets/logo.txt")
-      print logo
-
-      puts self.class::WELCOME_TEXT
-    end
-
-    # Launches the given event the the screen
-    # @param Event event The event to be launched on screen
-    # @return Event Next event to be launched on screen
-    def launch(event)
-
-      player_name = CodeWars::Player.instance.name || ""
-      player_input = nil
+    # Launches the events on the screen
+    # Recursive method accepting two params :
+    # @param [Event] event The event to be launched on screen
+    # @param [Decision] from_decision The decision that orginaly
+    # launched the event
+    # @return [Event] Next event to be launched on screen
+    def launch(event, from_decision=nil)
 
       # Shows event label
-      ap event.label
-
-      # Search for any decisions choices to provide
-      if (decisions = event.decisions).count > 0
-        player_attribute_decision = nil
-
-        if event.has_a_player_attribute_decision?
-          player_attribute_decision = event.decisions.first
-        else
-          decisions.each_with_index do |decision, i|
-            ap "#{i+1}/ #{decision.label}"
-          end
-        end
-
-        if player_attribute_decision
-          player_attribute_decision.update_player(player_input)
-        end
-
-        player_input = Readline.readline("#{player_name}❞ ", true)
+      unless event.available_decisions.count == 0 and
+        event.requires_all_decisions
+        ap event.label
       end
 
-      if !event.has_a_player_attribute_decision? and
-        player_input and player_input.size > 0
-
-        # Find selected decision
-        selected_decision = event.decisions[player_input.to_i]
-
-        # Maked it as made
-        selected_decision.made_at = Time.now
+      # Mark the decision as "made"
+      # We consider the decision as "made" once the event it leads to
+      # has been displayed on screen
+      if from_decision
+        from_decision.made_at = Time.now
       end
 
+      selected_decision, input_was_invalid = ask_player_to_make_decision(event)
 
-      if next_event = event.resolve_next_event
-        launch(next_event)
+      if !selected_decision and input_was_invalid
+        ap "Bye bye Jedi"
+        return
+      end
+      #
+      # if !event.has_a_player_attribute_decision? and
+      #   player_input and player_input.size > 0 and
+      #   available_decisions.count > 0
+      #
+      #   # Find selected decision
+      #   selected_decision = available_decisions[player_input.to_i - 1]
+      # end
+
+      if next_event = event.resolve_next_event(selected_decision)
+        launch(next_event, selected_decision)
       else
         ap "This the end !"
       end
@@ -107,6 +94,90 @@ module CodeWars
     end
 
     private
+
+    # Shows the game logo & welcome text
+    def welcome
+      logo = File.read("app/assets/logo.txt")
+      print logo
+
+      puts self.class::WELCOME_TEXT
+    end
+
+    # This methods :
+    # - shows the player available decisions
+    # - asks the player to choose the decision
+    # - (and prevents from entering something else !)
+    # - stores any player attribute if applyable
+    # @param [Event] event from which decisions are pulled off
+    # @return [Decision, Boolean] any chosen decision by player
+    def ask_player_to_make_decision(event)
+      selected_decision = nil
+      player_name = CodeWars::Player.instance.name || ""
+      player_input_is_valid = false
+
+      # Extracts available (non-already made) decisions
+      available_decisions = event.available_decisions
+
+      # Show decisions choices to player
+      if available_decisions.count > 0
+        player_attribute_decision = nil # store as a cache value any attribute decision
+
+        # If this is "player attribute" decision
+        # Then no need to loop over
+        if event.has_a_player_attribute_decision?
+          player_attribute_decision = event.decisions.first
+        else
+          available_decisions.each_with_index do |decision, i|
+            ap "#{i+1}/ #{decision.label}"
+          end
+        end
+
+        unless event.has_a_player_attribute_decision?
+          ap "--> Press the number of your choice below :"
+        end
+
+        max_attempts = 3 # we allow only 3 attempts - the game will stop if no decision is made
+        attempts = 0
+        while !player_input_is_valid and attempts < max_attempts do
+          player_input = Readline.readline("#{player_name}❞ ", true)
+          attempts += 1
+          player_input_is_valid = event.is_player_input_valid?(player_input)
+
+          if !player_input_is_valid
+            display_error_messages((attempts == max_attempts),
+              !event.has_a_player_attribute_decision?)
+          end
+        end
+
+        if player_input_is_valid
+          if player_attribute_decision
+            player_attribute_decision.update_player(player_input)
+          end
+
+          # Find selected decision
+          selected_decision = available_decisions[player_input.to_i - 1]
+        end
+
+        [selected_decision, player_input_is_valid]
+      end
+
+    end
+
+    # Shows display error messages
+    # @param [Boolean] has_exceeded_attempts
+    # @param [Boolean] is_a_numbered_input
+    # TODO: missing specs
+    def display_error_messages(has_exceeded_attempts, is_a_numbered_input)
+      if !has_exceeded_attempts
+        if is_a_numbered_input
+          ap "Please choose a valid number"
+        else
+          ap "Please enter something !"
+        end
+      else
+        ap "Alright - game is over |o| ..."
+      end
+    end
 
     DIRS_TO_LOAD = %w(config extensions models)
 
--- spec/lib/launcher_spec.rb
+++ spec/lib/launcher_spec.rb
@@ -35,12 +35,13 @@ RSpec.describe "Launcher" do
           allow_any_instance_of(CodeWars::Launcher).to receive(p_cmd.to_sym)
           .and_return(true)
         end
+        CodeWars::Launcher.disable_auto_start!
         launcher = CodeWars::Launcher.instance
       end
 
       # Checks console output
       expect do
-        launcher = launcher.welcome
+        launcher = launcher.send(:welcome)
       end.to output(/#{logo + CodeWars::Launcher::WELCOME_TEXT}/).to_stdout
     end
   end
@@ -51,6 +52,80 @@ RSpec.describe "Launcher" do
         CodeWars::Launcher.disable_auto_start!
         CodeWars::Launcher.instance
       end.to output("").to_stdout
+    end
+  end
+
+  describe "#launch" do
+
+  end
+
+  describe "#ask_player_to_make_decision" do
+    let(:e0_event) {
+      e0_event = CodeWars::DataStore.instance.events
+        .select{|e| e.slug == "E0"}.first
+    }
+    let(:e1_event) {
+      e1_event = CodeWars::DataStore.instance.events
+        .select{|e| e.slug == "E1"}.first
+    }
+    before(:each) do
+      CodeWars::Launcher.disable_auto_start!
+      allow(Readline).to receive(:readline).and_return("David")
+    end
+    context "with E1 event" do
+      it "shows decisions D1, D2, D3" do
+        expect do
+          CodeWars::Launcher
+            .instance.send(:ask_player_to_make_decision, e1_event)
+        end
+        .to output(/(Observe the details)/).to_stdout
+        expect do
+          CodeWars::Launcher
+            .instance.send(:ask_player_to_make_decision, e1_event)
+        end
+        .to output(/(Ask one of the custody that was present)/).to_stdout
+        expect do
+          CodeWars::Launcher
+            .instance.send(:ask_player_to_make_decision, e1_event)
+        end
+        .to output(/(Search some details on the ground)/).to_stdout
+      end
+      it "asks player to make a choice" do
+        expect do
+          CodeWars::Launcher
+            .instance.send(:ask_player_to_make_decision, e1_event)
+        end
+        .to output(/(Press the number of your choice below)/).to_stdout
+      end
+      it "prevents player from doing other choices than the ones allowed" do
+        invalid_values = ["David", "5", "10"]
+        invalid_values.each do |invalid_value|
+          expect do
+            allow(Readline).to receive(:readline).and_return(invalid_value)
+            CodeWars::Launcher
+            .instance.send(:ask_player_to_make_decision, e1_event)
+          end
+          .to output(/(Please choose a valid number)/).to_stdout
+        end
+
+        valid_values = [1,2,3].collect(&:to_s)
+        valid_values.each do |valid_value|
+          expect do
+            allow(Readline).to receive(:readline).and_return(valid_value)
+            CodeWars::Launcher
+              .instance.send(:ask_player_to_make_decision, e1_event)
+          end
+          .not_to output(/(Please choose a valid number)/).to_stdout
+        end
+      end
+    end
+
+    context "with E0 event that asks for player's name" do
+      it "stores player's name" do
+        CodeWars::Launcher
+          .instance.send(:ask_player_to_make_decision, e0_event)
+        expect(CodeWars::Player.instance.name).to eq("David")
+      end
     end
   end
 end
